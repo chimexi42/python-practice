f172b6b9ef8f1a9b78e9c78577c4cab750e048d0
Chima Okoro <chimaokoro42@gmail.com>
2022-04-09 22:28:04 +0100

    new tf

---
+++ tf2.py
@@ -0,0 +1,53 @@
+from pydoc import classname
+import tensorflow as tf
+from tensorflow import keras
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+data = tf.keras.datasets.fashion_mnist
+
+(train_images, train_labels), (test_images, test_labels) = data.load_data()
+
+class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat', 'Sandal',
+               'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+train_images = train_images/255.0
+test_images = test_images/255.0
+
+print(train_labels[7])
+print(train_images[7])
+
+
+plt.imshow(train_images[7], cmap=plt.cm.binary)
+# plt.show()
+
+# define the model
+
+model = keras.Sequential([
+    keras.layers.Flatten(input_shape=(28, 28)),
+    keras.layers.Dense(128, activation='relu'),
+    keras.layers.Dense(10, activation='softmax')
+])
+
+model.compile(optimizer='adam',
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+
+model.fit(train_images, train_labels, epochs=10)
+
+test_loss, test_acc = model.evaluate(test_images, test_labels)
+
+print('Loss:', test_loss)
+print('Accuracy:', test_acc)
+
+
+prediction = model.predict(test_images)
+
+print(prediction[0])
+for i in range(5):
+    plt.imshow(test_images[i], cmap=plt.cm.binary)
+    plt.xlabel('Actual: ' + class_names[test_labels[i]])
+    plt.title('Prediction', class_names[np.argmax(prediction[i])])
+    plt.show()
+
+print(class_names[np.argmax(prediction[1])])
